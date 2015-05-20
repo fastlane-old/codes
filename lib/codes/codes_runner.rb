74704a85cbe1a6f94bc7b718661460bfb7b5b144
Jerome Lacoste <jerome.lacoste@gmail.com>
2015-05-20 11:52:30 +0200

    Adding a command to display the number of remaining codes

--- lib/codes/codes_runner.rb
+++ lib/codes/codes_runner.rb
@@ -3,5 +3,8 @@ module Codes
     def self.download(args)
       Codes::ItunesConnect.new.download args
     end
+    def self.display(args)
+      Codes::ItunesConnect.new.display args
+    end
   end
 end
--- lib/codes/itunes_connect.rb
+++ lib/codes/itunes_connect.rb
@@ -65,6 +65,45 @@ module Codes
       puts codes
     end
 
+    def display(args)
+      Helper.log.info "Displaying remaining number of codes promo"
+
+      app_id = args[:apple_id]
+      app_id ||= (FastlaneCore::ItunesSearchApi.fetch_by_identifier(args[:app_identifier])['trackId'] rescue nil)
+
+      app_identifier = args[:app_identifier]
+
+      if app_id.to_i == 0 or app_identifier.to_s.length == 0
+        raise "Could not find app using the following information: #{args}. Maybe the app is not in the store. Pass the Apple ID of the app as well!".red
+      end
+
+      app = FastlaneCore::ItunesSearchApi.fetch(app_id)
+      platform = (app['kind'] == "mac-software" ? "osx" : "ios")
+
+      # Use Pathname because it correctly handles the distinction between relative paths vs. absolute paths
+      output_file_path = Pathname.new(args[:output_file_path]) if args[:output_file_path]
+      output_file_path ||= Pathname.new(File.join(Dir.getwd, "#{app_identifier || app_id}_codes_info.txt"))
+      raise "Insufficient permissions to write to output file".red if File.exists?(output_file_path) and not File.writable?(output_file_path)
+      visit PROMO_URL.gsub("[[app_id]]", app_id.to_s).gsub("[[platform]]", platform)
+
+      begin
+        text_fields = wait_for_elements("input[type=text]")
+      rescue
+        raise "Could not open details page for app #{app_identifier}. Are you sure you are using the correct apple account and have access to this app?".red
+      end
+      raise "There should only be a single text input field to specify the number of codes".red unless text_fields.count == 1
+
+      remaining_divs=wait_for_elements("div#codes_0")
+      raise "There should only be a single text div containing the number of remaining codes".red unless remaining_divs.count == 1
+      remaining=remaining_divs.first.text.split(" ")[0]
+
+      bytes_written = File.write(output_file_path.to_s, remaining, mode: "a+")
+      Helper.log.warn "Could not write your codes to the codes_info.txt file, but you can still access them from iTunes Connect later" if bytes_written == 0
+      Helper.log.info "Added generated codes to '#{output_file_path.to_s}'".green unless  bytes_written == 0
+
+      puts remaining
+    end
+
     def click_next
       wait_for_elements("input.continueActionButton").first.click
     end
