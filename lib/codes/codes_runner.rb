43348815ae1c9d297e2ca57dc21a51038b9cebf6
KrauseFx <KrauseFx@gmail.com>
2015-09-07 13:26:59 -0700

    Added missing newline

--- lib/codes/codes_runner.rb
+++ lib/codes/codes_runner.rb
@@ -3,6 +3,7 @@ module Codes
     def self.download(args)
       Codes::ItunesConnect.new.download args
     end
+    
     def self.display(args)
       Codes::ItunesConnect.new.display args
     end
